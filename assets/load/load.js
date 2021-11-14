3d5501eb8593cf92faec769084610634f0d40c8b
nxxh447 <65509800+nxxh447@users.noreply.github.com>
2021-11-13 19:23:05 -0500

    ..

---
+++ assets/load/load.js
@@ -0,0 +1,3 @@
+setTimeout(() => { document.getElementById("spinner").style.display = "none"; },3000);
+setTimeout(()=> { document.getElementById("mIcon").style.display = "block"; }, 3000);
+setTimeout(() => { document.getElementById("contact").style.display = "block"; }, 3000);
