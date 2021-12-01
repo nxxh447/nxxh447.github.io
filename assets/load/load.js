45bb0e0d0b57f5feaf891d030dbbc94da7529d62
nxxh <65509800+nxxh447@users.noreply.github.com>
2021-12-01 13:23:53 -0500

    ..

--- assets/load/load.js
+++ assets/load/load.js
@@ -16,7 +16,7 @@ setTimeout(function() {
 }, 3000);
 */
 
-setTimeout(function(v) { this.style.display = "none" }(document.getElementById("spinner")));
+setTimeout(function(v) { let v = this; this.style.display = "none" }(document.getElementById("spinner")));
 setTimeout(function() {
   const elms = [document.getElementById("mIcon"), document.getElementById("contact")];
   elms.forEach(function(item) {
