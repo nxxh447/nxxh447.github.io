37656bc185e7621b67a25c42d8e62c9672d95bdc
nxxh <65509800+nxxh447@users.noreply.github.com>
2021-12-01 11:48:24 -0500

    ..

--- assets/newEditor.js
+++ assets/newEditor.js
@@ -1,67 +1,74 @@
-const el = document.querySelector(".editor");
 const js = el => {
   for (const node of el.children) {
     const s = node.innerText
-      .replace(/(\/\/.*)/g, "<em>$1</em>")
+      .replace(/(\/\/.*)/g, '<em>$1</em>')
       .replace(
         /\b(new|if|else|do|while|switch|for|in|of|continue|break|return|typeof|function|var|const|let|\.length|\.\w+)(?=[^\w])/g,
-        "<strong>$1</strong>'"
+        '<span>$1</span>',
       )
-      .replace(/(".*?"|'.*?'|`.*?`)/g, "<strong><em>$1</em></strong>")
-      .replace(/\b(\d+)/g, "<em><strong>$1</strong></em>");
-    node.innerHTML = s.split("\n").join("<br/>");
+      .replace(/(".*?"|'.*?'|`.*?`)/g, '<strong><em>$1</em></strong>')
+      .replace(/\b(\d+)/g, '<em><strong>$1</strong></em>');
+    node.innerHTML = s.split('\n').join('<br/>');
   }
 };
 
-const caret = el => {
-  const range = window.getSelection().getRangeAt(0);
-  const prefix = range.cloneRange();
-  
-  prefix.selectNodeContents(el);
-  prefix.setEnd(range.endContainer, range.endOffset);
-  
-  return prefix.toString().length;
-}; el.focus(); editor(el);
-
-const setCaret = (pos, parent) => {
-  for (const node of parent.childNodes) {
-    if (node.nodeType == Node.TEXT_NODE) {
-      if (node.length >= pos) {
-        const range = document.createRange();
-        const sel = window.getSelection();
-        
-        range.setStart(node, pos);
-        range.collapse(true);
-        sel.removeAllRanges();
-        sel.addRange(range);
-        
-        return -1;
-      } else { pos = pos - node.length; }
-    } else {
-      pos = setCaret(pos, node);
-      if (pos < 0) return pos;
-    }
-  }; return pos;
-};
-
-el.addEventListener("keyup", e => {
-  if (e.keyCode >= 0x30 || e.keyCode == 0x20) {
-    const pos = caret();
+const editor = (el, highlight = js, tab = '    ') => {
+  const caret = () => {
+    const range = window.getSelection().getRangeAt(0);
+    const prefix = range.cloneRange();
     
-    highlight(el);
-    setCaret(pos);
-  }
-});
+    prefix.selectNodeContents(el);
+    prefix.setEnd(range.endContainer, range.endOffset);
+    
+    return prefix.toString().length;
+  };
 
-/*
-fnAstyle = function attribute style
-decStyle = declatarive element style
-cmtStyle = comment style
-*/
+  const setCaret = (pos, parent = el) => {
+    for (const node of parent.childNodes) {
+      if (node.nodeType == Node.TEXT_NODE) {
+        if (node.length >= pos) {
+          const range = document.createRange();
+          const sel = window.getSelection();
+          
+          range.setStart(node, pos);
+          range.collapse(true);
+          sel.removeAllRanges();
+          sel.addRange(range);
+          
+          return -1;
+        } else { pos = pos - node.length; }
+      } else {
+        pos = setCaret(pos, node);
+        if (pos < 0) { return pos; }
+      }
+    }
+    
+    return pos;
+  };
 
-document.getElementsByTagName("span").setAttributeNode(document.createAttribute("class").value = "fnAttr"); // tmp
+  highlight(el);
 
+  el.addEventListener('keydown', e => {
+    if (e.which === 9) {
+      const pos = caret() + tab.length;
+      const range = window.getSelection().getRangeAt(0);
+      range.deleteContents();
+      range.insertNode(document.createTextNode(tab));
+      highlight(el);
+      setCaret(pos);
+      e.preventDefault();
+    }
+  });
 
-document.getElementsByTagName("fn-attr-elm").setAttributeNode(document.createAttribute("class").value = "fnAstyle");
-document.getElementsByTagName("dec-elm").setAttributeNode(document.createAttribute("class").value = "decStyle");
-document.getElementsByTagName("cmt-elm").setAttributeNode(document.createAttribute("class".value = "cmtStyle"))
+  el.addEventListener('keyup', e => {
+    if (e.keyCode >= 0x30 || e.keyCode == 0x20) {
+      const pos = caret();
+      highlight(el);
+      setCaret(pos);
+    }
+  });
+};
+
+const el = document.querySelector('.editor');
+el.focus();
+editor(el);
