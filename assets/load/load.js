5146a20dc7d1374095c4be637e60cd720f873e9a
nxxh <65509800+nxxh447@users.noreply.github.com>
2021-12-01 13:53:39 -0500

    ..

--- assets/load/load.js
+++ assets/load/load.js
@@ -1,7 +1,9 @@
 setInterval(function(v) { v.style.display = "none" }(document.getElementById("spinner")), 3000)
 setInterval(function() {
-  const a = document.getElementById("mIcon");
+  const a = document.getElementById("mIcon")
   const b = document.getElementById("contact");
+  const c = style.display;
   
-  ((a && b).style.display = "block")
+  a.c = "block"
+  b.c = "block"
 }, 3000);
