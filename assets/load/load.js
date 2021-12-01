4929ae6ecc37b145bdf34ab6fe046b50b21a99a9
nxxh <65509800+nxxh447@users.noreply.github.com>
2021-12-01 14:04:17 -0500

    ..

--- assets/load/load.js
+++ assets/load/load.js
@@ -1,8 +1,4 @@
-function loadFn(elmName, dispType) {
-  return setInterval(function() {
-    document.getElementById(elmName).style.display = dispType;
-  }, 3000)
-}
+const loadFn = (elmName, dispType) => { setInterval(function() { document.getElementById(elmName).style.display = dispType }, 3000); }
 
 loadFn("spinner", "none");
 loadFn("mIcon", "block");
