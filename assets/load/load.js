8442b14775cbbbe6e9aba8feb88c084d9ef8ae91
nxxh <65509800+nxxh447@users.noreply.github.com>
2021-12-01 14:00:44 -0500

    ..

--- assets/load/load.js
+++ assets/load/load.js
@@ -1,9 +1,9 @@
-setInterval(function() { document.getElementById("spinner").style.display = "none" }, 3000);
-setInterval(function() {
-  const a = document.getElementById("mIcon")
-  const b = document.getElementById("contact");
-  const c = style.display;
-  
-  a.c = "block"
-  b.c = "block"
-}, 3000);
+function loadFn(elm, style) {
+  return setInterval(() => {
+    document.getElementById(elm).style.display = style;
+  }, 3000)
+}
+
+loadFn("spinner", "none");
+loadFn("mIcon", "block");
+loadFn("contact", "block");
