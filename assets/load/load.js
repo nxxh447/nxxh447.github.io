29672d00c077645e95186d1be8182291352038c0
nxxh <65509800+nxxh447@users.noreply.github.com>
2021-12-01 13:24:29 -0500

    ..

--- assets/load/load.js
+++ assets/load/load.js
@@ -1,21 +1,3 @@
-/*
-setTimeout(() => { document.getElementById("spinner").style.display = "none"; },3000);
-setTimeout(()=> { document.getElementById("mIcon").style.display = "block"; }, 3000);
-setTimeout(() => { document.getElementById("contact").style.display = "block"; }, 3000);
-*/
-
-/*
-setTimeout(function() {
-  const ge = (v) => { return document.getElementById(v); }
-  const elms = [ge("mIcon"),ge("contact")];
-  const oelm = ge("spinner");
-  
-  elms.forEeach(function(item) {
-    this.style.display = "block";
-  }); oelm.style.display = "none";
-}, 3000);
-*/
-
 setTimeout(function(v) { let v = this; this.style.display = "none" }(document.getElementById("spinner")));
 setTimeout(function() {
   const elms = [document.getElementById("mIcon"), document.getElementById("contact")];
