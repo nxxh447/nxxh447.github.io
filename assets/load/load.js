ee7cadb2c859b19a4d3e3e5934fa808ba709df32
nxxh <65509800+nxxh447@users.noreply.github.com>
2021-12-01 13:34:29 -0500

    ..

--- assets/load/load.js
+++ assets/load/load.js
@@ -1,8 +1,7 @@
-setTimeout(function(v) { let v = this; this.style.display = "none" }(document.getElementById("spinner")));
+setTimeout(function(v) { v.style.display = "none" }(document.getElementById("spinner")), 3000);
 setTimeout(function() {
   const elms = { document.getElementById("mIcon"), document.getElementById("contact") };
   elms.forEach(function(item) {
-    let this = item;
-    this.style.display = "block"
+    item.style.display = "block"
   });
-});
+}, 3000);
