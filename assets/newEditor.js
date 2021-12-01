547874e4e5a2fc835e2c6df55547c851b7cddb6b
nxxh <65509800+nxxh447@users.noreply.github.com>
2021-12-01 12:57:26 -0500

    ..

--- assets/newEditor.js
+++
@@ -1,7 +0,0 @@
-CodeMirror(document.getElementById("editorContainer"), {
-  lineNumbers: true,
-  tabSize: 2,
-  value: '// editor version (beta) :) \nconsole.log("Hello, World");',
-  mode: "javascript",
-  theme: "monokai"
-});
