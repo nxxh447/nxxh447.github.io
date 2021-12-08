f086a36d28d27a2a00e3325c4868b6f4232211ae
nxxh <65509800+nxxh447@users.noreply.github.com>
2021-12-08 12:29:18 -0500

    ..

--- assets/wh-assets/whSender.js
+++
@@ -1,7 +0,0 @@
-function sendToWh(botNm, msgContent) {
-  const whArgData = { username: `${botNm}`, avatar_url: "", content: `${msgContent}` };
-  const request = new XMLHttpRequest();
-  
-  request.open("POST", `${msgContent}`).setRequestHeader("Content-type","application/json");
-  request.send(JSON.stringify(whArgData))
-}
