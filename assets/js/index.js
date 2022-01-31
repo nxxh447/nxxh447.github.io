2606852d7e84e328591c10a1619f44926b57310c
nxxh447 <65509800+nxxh447@users.noreply.github.com>
2022-01-31 10:19:29 -0500

    ..

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,4 @@
-console.log(document.title);
+const detectOS = () => {
+    if (!navigator.appVersion.indexOf("Win") != -1) return document.getElementByTag("body").style.display = "none!important";
+    if (!navigator.appVersion.indexOf("Mac") != -1) return document.getElementByTag("body").style.display = "none!important";
+}
