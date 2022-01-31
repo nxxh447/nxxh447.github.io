f22e74278e6ef232bec08506fdc3e9f0532fff33
nxxh <65509800+nxxh447@users.noreply.github.com>
2022-01-31 14:05:17 -0800

    ..

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,1 @@
-const detectOS = () => {
-    if (!navigator.appVersion.indexOf("Win") != -1) return document.getElementByTag("body").style.display = "none!important";
-    if (!navigator.appVersion.indexOf("Mac") != -1) return document.getElementByTag("body").style.display = "none!important";
-}
+console.log("test...");
