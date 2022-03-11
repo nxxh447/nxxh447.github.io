83f1a8e8980068ca4cf9ecda2a17ef777a1e106c
nxxh447 <65509800+nxxh447@users.noreply.github.com>
2022-03-11 12:32:55 -0500

    Update redirect.js

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,2 +1,8 @@
-const sleep = (ms) => { return new Promise((res) => setTimeout(res, ms)) }
-sleep(2000).then(() => window.location.replace("https://savelife.in.ua/en/donate"));
+(function() {
+	sleep(2000).then(() => window.location.replace("https://savelife.in.ua/en/donate"))
+	function sleep(ms) {
+		return new Promise((res) => {
+			setTimeout(res, ms);
+		});
+	}
+})();
