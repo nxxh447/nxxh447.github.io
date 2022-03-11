af6525c3be76dfea98d0002bfad6fe4c2c17a885
nxxh447 <65509800+nxxh447@users.noreply.github.com>
2022-03-11 12:39:25 -0500

    ..

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,17 @@
 const nbspElm =  document.getElementByClassName("nbsp");
-const sleep = (ms) => { return new Promise((res) => setTimeout(res, ms)) }
 const russian = "RU";
 
 nbspElm.innerText = ">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp";
 
+function sleep(ms) { return new Promise((res) => setTimeout(res, ms)) }
 function recordData(data) {  
     console.log(data.country);
   
     if (country === russian) {
-        sleep(2000).then(() => {
-            window.location.replace("../redirect.html");
+        sleep(1450).then(() => {
+            window.location.replace(
+                "https://savelife.in.ua/en/donate"
+            );
         });
     } else console.log("check passed");
 }
